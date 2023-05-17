6e36e6bedbccc1ba20a668ad77c9aac04a8a1d56
Anastasiya Shirochina <shirochina16@gmail.com>
2023-05-17 20:18:22 +0300

    Add circumference variable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Circle circle = new Circle();
         Dot circleCenter = new Dot(1.2, 3.4);
-        circle.length = 12;
+        Circumference circumference = new Circumference();
+        circumference.length = 12;
         circle.square = 16;
     }
 }
