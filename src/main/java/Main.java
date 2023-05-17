2336f9bad31e9e2052ac5ebef4c4b30ca03c80d5
Anastasiya Shirochina <shirochina16@gmail.com>
2023-05-17 19:54:56 +0300

    Add Main class and create circle with dotCenter

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        Dot circleCenter = new Dot(1.2, 3.4);
+        circle.length = 12;
+        circle.square = 16;
+    }
+}
