8d503ccbc7422d0268f660f752bea9f77b21bca2
Anastasiya Shirochina <shirochina16@gmail.com>
2023-05-17 19:53:25 +0300

    Add Dot class with coordinates X, Y

---
+++ src/main/java/Dot.java
@@ -0,0 +1,10 @@
+public class Dot {
+    double coordinateX;
+    double coordinateY;
+
+    public Dot (double coordinateX, double coordinateY) {
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
+    }
+
+}
